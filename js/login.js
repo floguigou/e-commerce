f7a18ef24bae1ae2e36b083eec10aa6fe2527544
floguigou <flo-guigou@hotmail.com>
2022-08-13 22:32:51 -0300

    Login y redireccion

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.getElementById("button_login").addEventListener("click", function () {
+    let email = document.getElementById ("email").value; 
+    let password = document.getElementById ("password"). value; 
+
+    if (email && password) {
+        //console.log ("redireccionar")
+        window.location.href = "index.html"
+    } 
+})
