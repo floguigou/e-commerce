11970778403132529871a73ddbdd91aa5ab5f757
floguigou <flo-guigou@hotmail.com>
2022-09-13 15:47:01 -0300

    Entrega 3 modificaciones

--- js/product-info.js
+++ js/product-info.js
@@ -1,72 +1,108 @@
-let productinfo = `https://japceibal.github.io/emercado-api/products/${localStorage.getItem("productID")}.json`
-let infocomments = `https://japceibal.github.io/emercado-api/products_comments/${localStorage.getItem("productID")}.json`
+let productinfo = `https://japceibal.github.io/emercado-api/products/${localStorage.getItem("productID")}.json`;
+let infocomments = `https://japceibal.github.io/emercado-api/products_comments/${localStorage.getItem("productID")}.json`;
+let comments = [];
 
+function obtener_localStorage() {
+    let mostrarEmail = localStorage.getItem("usuario");
+    return mostrarEmail;
+}
 
-document.addEventListener("DOMContentLoaded", function(evt){
+document.addEventListener("DOMContentLoaded", function (evt) {
+    getJSONData(productinfo).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            showProductsInfoList(resultObj.data);
+        }
+    });
 
-    getJSONData(productinfo)
-    .then(function(resultObj){
-        if (resultObj.status === "ok"){
-            showProductsInfoList(resultObj.data)
+    getJSONData(infocomments).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            comments = resultObj.data;
+            showProductsComments(comments);
         }
     });
 });
 
-function showProductsInfoList (product) {
-    let imgProducts = ""
-for(let img of product.images) {
-    imgProducts +=`
-    <img src="${img}"/>`
+function showProductsInfoList(product) {
+    let imgProducts = "";
+    for (let img of product.images) {
+        imgProducts += `<img class="col-2 border m-2" src="${img}"/>`;
+    }
+
+    let products = `<h2> ${product.name} </h2>
+    <hr> 
+    <h4><strong> Precio </strong></h4>
+    <p> ${product.currency} ${product.cost} </p>
+    <br>
+    <h4><strong> Descripción </strong></h4>
+    <p> ${product.description} </p>
+    <br>
+    <h4><strong> Categoría </strong></h4>
+    <p> ${product.category} </p>
+    <br>
+    <h4><strong> Cantidad vendida </strong></h4>
+    <p> ${product.soldCount} </p>
+    <br>
+    <h4><strong> Imágenes ilustrativas </strong></h4>
+    <div class="d-flex justify-content-center">
+        <div class="row"> 
+            ${imgProducts}
+        </div>
+    </div>
+    `;
+    document.getElementById("info-product").innerHTML = products;
 }
 
-   let products = `<h2> ${product.name} </h2>
-   <hr> 
-   <h4><strong> Precio </strong></h4>
-   <p> ${product.currency} ${product.cost} </p>
-   <br>
-   <h4><strong> Descripción </strong></h4>
-   <p> ${product.description} </p>
-   <br>
-   <h4><strong> Categoría </strong></h4>
-   <p> ${product.category} </p>
-   <br>
-   <h4><strong> Cantidad vendida </strong></h4>
-   <p> ${product.soldCount} </p>
-   <br>
-   <h4><strong> Imágenes ilustrativas </strong></h4>
-   <div>
-   <p> ${imgProducts} </p>
-   </div>
-   `
-   document.getElementById("info-product").innerHTML = products;
-}
-
-
-document.addEventListener("DOMContentLoaded", function(evt){
-
-    getJSONData(infocomments)
-    .then(function(resultObj){
-        if (resultObj.status === "ok"){
-            showProductsComments(resultObj.data)
+const stars = (score) => {
+    let star = "";
+    for (let s = 1; s <= 5; s++) {
+        if (s <= score) {
+            star += '<span class="fa fa-star checked"></span>';
+        } else {
+            star += '<span class="fa fa-star"></span>';
         }
-    });
-});
+    }
+    return star;
+};
 
-function showProductsComments(infocomments){
-    let comments = "";
-
-    for(let comment of infocomments ){ 
-            comments +=`
-                <div class="row">
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <div class="mb-1">
-                            <h4> ${comment.user} ${comment.dateTime} ${comment.score} </h4> 
-                            <p> ${comment.description} </p> 
+function showProductsComments(listComments) {
+    let productComments = "";
+    for (let comment of listComments) {
+        productComments += `
+                    <div class="row">
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <div class="mb-1">
+                                <h4> ${comment.user} ${comment.dateTime} ${stars(comment.score)}</h4> 
+                                <p> ${comment.description} </p> 
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>`
-        }
-        document.getElementById("comments-product").innerHTML = comments;
+                    </div>`;
+        //console.log(comment)
     }
+    document.getElementById("comments-product").innerHTML = productComments;
+}
+
+const formatYmd = (date) => date.toISOString().slice(0, 10) + " " + date.toTimeString().slice(0, 8);
+
+let buttonEnviar = document.getElementById("enviar");
+
+buttonEnviar.addEventListener("click", (evt) => {
+    let inputText = document.getElementById("opinion");
+    let puntuacion = document.getElementById("puntuacion");
+
+    let newComment = {
+        user: obtener_localStorage(),
+        description: inputText.value,
+        score: puntuacion.value,
+        dateTime: formatYmd(new Date),
+        product: localStorage.getItem("productID"),
+    }
+
+    comments.push(newComment);
+
+    showProductsComments(comments);
+
+    inputText.value = "";
+    puntuacion.value = 1;
+});
